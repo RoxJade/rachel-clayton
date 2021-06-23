5af05b294955680fbd8ae00ce8107f8efe63a376
RoxJade <roxannejade88@icloud.com>
2021-06-23 17:37:40 +0000

    Add Stripe functionality to checkout

--- rachel_clayton/settings.py
+++ rachel_clayton/settings.py
@@ -173,4 +173,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Stripe
 UK_VAT_PERCENTAGE = 20
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
