8ec35ecb910751e29a69d61eed487f70b838a574
RoxJade <roxannejade88@icloud.com>
2021-06-23 16:50:18 +0000

    Add stipe elements js and styling from stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Iz0uBKgP7mMfw1vw7e4vNw6jbYtMGceFKmZQPBGJGu47zvWTl2GUEZTlXSMr8eZdlsVfwciPMU22Wp5d3dVS9Vm00r4OeTRWE',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
