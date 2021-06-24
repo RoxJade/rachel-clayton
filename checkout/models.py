fa881eec91a28d00d3d85e4912bd89b5eae048fd
RoxJade <roxannejade88@icloud.com>
2021-06-24 15:14:56 +0000

    Completed webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -8,7 +8,6 @@ from products.models import Product
 
 
 class Order(models.Model):
-
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -26,6 +25,8 @@ class Order(models.Model):
         max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
