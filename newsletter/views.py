6d93f1a00ed012f6369195865e5efbc2bbcc4cea
RoxJade <roxannejade88@icloud.com>
2021-06-27 02:29:46 +0000

    styled the home page and updated links

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,8 +1,6 @@
-
 from django.conf import settings
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from .forms import EmailSignupForm
 from .models import Signup
 
