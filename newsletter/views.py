827e966d8982b7213436326145ec66b4713e3f04
RoxJade <roxannejade88@icloud.com>
2021-06-28 19:54:42 +0000

    Bug fix in views.py for newsletter subscription

--- newsletter/views.py
+++ newsletter/views.py
@@ -11,17 +11,14 @@ MAILCHIMP_API_KEY = settings.MAILCHIMP_API_KEY
 MAILCHIMP_DATA_CENTER = settings.MAILCHIMP_DATA_CENTER
 MAILCHIMP_EMAIL_LIST_ID = settings.MAILCHIMP_EMAIL_LIST_ID
 
-api_url = 'https://{dc}.api.mailchimp.com/3.0'.format(dc=MAILCHIMP_DATA_CENTER)
-members_endpoint = '{api_url}/lists/{list_id}/members'.format(
-    api_url=api_url,
-    list_id=MAILCHIMP_EMAIL_LIST_ID
-)
+api_url = f'https://{MAILCHIMP_DATA_CENTER}.api.mailchimp.com/3.0'
+members_endpoint = f'{api_url}/lists/{MAILCHIMP_EMAIL_LIST_ID}/members'
 
 
 def subscribe(email):
     data = {
-        "email_address": email,
-        "status": "subscribed"
+        'email_address': email,
+        'status': 'subscribed'
     }
     r = requests.post(
         members_endpoint,
@@ -32,7 +29,9 @@ def subscribe(email):
 
 
 def email_list_signup(request):
+
     form = EmailSignupForm(request.POST or None)
+
     if request.method == "POST":
         if form.is_valid():
             email_signup_qs = Signup.objects.filter(email=form.instance.email)
