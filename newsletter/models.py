79eb23255dabe9f92a2a979b98a1343e639c139a
RoxJade <roxannejade88@icloud.com>
2021-06-28 22:46:22 +0000

    Fixed a button I broke earlier!

--- newsletter/models.py
+++ newsletter/models.py
@@ -6,7 +6,7 @@ class Signup(models.Model):
     class Meta:
         verbose_name_plural = 'Subscribers'
 
-    email = models.EmailField(max_length=254, null=False, blank=False)
+    email = models.EmailField()
     timestamp = models.DateTimeField(auto_now_add=True,)
 
     def __str__(self):
